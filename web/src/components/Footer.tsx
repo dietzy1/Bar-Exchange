6636c0c6f5115eb815d5985f2bcd31f8bd888fa4
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-08-13 21:22:11 +0200

    Feat: Footer

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,39 @@
+/** @format */
+
+import { AiOutlineStock } from "react-icons/ai";
+
+import Timer from "./Timer";
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="px-6 pb-4 w-screen mt-auto">
+      <div className="text-1xl font-bold whitespace-nowrap text-white flex items-center justify-between pl-14 pr-14">
+        <div className=" flex items-center text-black">
+          <AiOutlineStock className="h-8 w-8 mr-2 text-black" />
+          BØRSBAR
+        </div>
+
+        <div>
+          <div className="flex flex-col items-center">
+            <h1 className="text-2xl md:text-4xl font-bold mb-2 text-black">
+              <span className="text-green-500">Børsen</span> lukker om
+            </h1>
+          </div>
+          <div className="flex justify-center align-middle">
+            <Timer totalHours={6} />
+          </div>
+        </div>
+
+        <div className=" flex items-center text-black invisible">
+          <AiOutlineStock className="h-8 w-8 mr-2 text-black" />
+          BØRSBAR
+        </div>
+      </div>
+
+      <hr className="my-3 sm:mx-auto  " />
+      <span className="block text-sm text-gray-400 sm:text-center ">
+        © 2023 BØRSBAR™
+      </span>
+    </footer>
+  );
+}
