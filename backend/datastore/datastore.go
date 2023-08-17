26e7d6bcd574bf56ed23371c8eea88c8fcdf3f1c
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-08-17 21:30:49 +0200

    feat: db init

---
+++ backend/datastore/datastore.go
@@ -0,0 +1,45 @@
+package datastore
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type db struct {
+	client *mongo.Client
+}
+
+type Config struct {
+	URI     string
+	Timeout time.Duration
+}
+
+func New(config *Config) (*db, error) {
+
+	if config.Timeout == 0 {
+		config.Timeout = 5 * time.Second
+	}
+
+	if config.URI == "" {
+		return nil, fmt.Errorf("URI is empty, missing connection string")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), config.Timeout)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(config.URI))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to mongo: %v", err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping mongo: %v", err)
+	}
+	c := &db{client: client}
+	return c, nil
+
+}
