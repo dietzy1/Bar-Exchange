1611c6faa4172a623f949acd3afd81793e45ba53
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-08-18 12:46:41 +0200

    feat: db event boilerplate

--- backend/datastore/event.go
+++ backend/datastore/event.go
@@ -1,1 +1,19 @@
 package datastore
+
+import (
+	"context"
+
+	"github.com/dietzy1/Bar-Exchange/service"
+)
+
+func (d *db) StartEvent(ctx context.Context, req service.Event) (service.Event, error) {
+	return service.Event{}, nil
+}
+
+func (d *db) StopEvent(ctx context.Context, req service.Event) (service.Event, error) {
+	return service.Event{}, nil
+}
+
+func (d *db) GetEvent(ctx context.Context, req service.Event) (service.Event, error) {
+	return service.Event{}, nil
+}
