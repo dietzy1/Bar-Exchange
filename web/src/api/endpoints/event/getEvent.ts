4cc334491c8bf8dfe355aa3b2ddf95001910634b
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-08-24 11:03:08 +0200

    feat: api request for getEvent

---
+++ web/src/api/endpoints/event/getEvent.ts
@@ -0,0 +1,36 @@
+/** @format */
+import ENV from "@/constants/env";
+import {
+  GetEventRequest,
+  GetEventResponse,
+} from "@/api/protos/v1/event/event_pb";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { http } from "@/api/axios";
+
+if (ENV.MOCKED) {
+  await import("./getEvent.mock.ts");
+}
+
+//This function I might actually be able to
+const GetEvent = async (req: GetEventRequest) => {
+  const { data } = await http.get(`v1/event/${req.id}`);
+
+  return data as GetEventResponse;
+};
+
+const useGetEvent = (req: GetEventRequest) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["useGetEvent"],
+    queryFn: () => GetEvent(req),
+  });
+
+  if (error) {
+    console.error(error);
+  }
+
+  return { data, isLoading, error };
+};
+
+export default useGetEvent;
