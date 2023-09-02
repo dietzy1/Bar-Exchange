d7183173c1c97a55b5a22c2db5e024aa67ba2d91
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-09-02 23:21:08 +0200

    feat: untested aggregate needs fix

--- backend/datastore/exchange.go
+++ backend/datastore/exchange.go
@@ -19,17 +19,27 @@ func (d *db) Store(ctx context.Context, req service.Beverage) error {
 
 }
 
-func (d *db) Aggregate(ctx context.Context) ([]service.Beverage, error) {
+// Fucntion which aggregates all of the purchases within a given time frame of 5 minutes of the timestamp
+func (d *db) Aggregate(ctx context.Context, timestamp string) ([]service.Beverage, error) {
 
+	//Create a slice of service.Beverage
 	var beverages []service.Beverage
-	cursor, err := d.client.Database(datastore).Collection(exchangeCollection).Find(ctx, bson.M{})
+	//Create a filter to find all of the purchases within a given time frame
+	filter := bson.M{"timestamp": bson.M{"$gte": timestamp, "$lt": timestamp}}
+
+	//Find all of the purchases within a given time frame
+	cursor, err := d.client.Database(datastore).Collection(exchangeCollection).Find(ctx, filter)
 	if err != nil {
 		return nil, err
 	}
-	if err := cursor.All(ctx, &beverages); err != nil {
-		return nil, err
+	//Iterate through the cursor and append each beverage to the slice
+	for cursor.Next(ctx) {
+		var beverage service.Beverage
+		if err := cursor.Decode(&beverage); err != nil {
+			return nil, err
+		}
+		beverages = append(beverages, beverage)
 	}
-
 	return beverages, nil
 
 }
