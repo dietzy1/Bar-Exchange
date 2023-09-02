9885de7e8dc4b3a4c115382e432ed211815c3d69
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-09-02 23:21:42 +0200

    feat: price calc configuration

--- backend/exchange/configuration.go
+++ backend/exchange/configuration.go
@@ -1,1 +1,18 @@
 package exchange
+
+import "time"
+
+type configuration struct {
+	timeframe        int           // 5 minutes
+	priceMultiplier  float64       // 1.5
+	intervalDuration time.Duration // 1 minute
+
+}
+
+func newBaseConfiguration() *configuration {
+	return &configuration{
+		timeframe:        5,
+		priceMultiplier:  1.5,
+		intervalDuration: time.Minute,
+	}
+}
