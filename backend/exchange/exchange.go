df61eced9bd6f02617a8f80876363060e147699a
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-09-02 23:22:10 +0200

    feat: Ensure no leaking goroutine

--- backend/exchange/exchange.go
+++ backend/exchange/exchange.go
@@ -17,6 +17,15 @@ type exchange struct {
 	broker Broker
 
 	store dataStore
+
+	stopSimulationChan chan struct{} // Channel to signal stop simulation
+
+	conf configuration
+}
+
+type dataStore interface {
+	Store(ctx context.Context, req service.Beverage) error
+	Aggregate(ctx context.Context, timestamp string) ([]service.Beverage, error)
 }
 
 type Broker interface {
@@ -25,20 +34,23 @@ type Broker interface {
 	Publish(ctx context.Context, message []byte) error
 }
 
-type dataStore interface {
-	Store(ctx context.Context, req service.Beverage) error
-	aggregate(ctx context.Context) ([]service.Beverage, error)
-}
+func New(logger *zap.Logger, broker Broker, store dataStore) (*exchange, error) {
 
-func New(logger *zap.Logger, broker Broker, store dataStore) *exchange {
+	if broker == nil {
+		return nil, fmt.Errorf("Broker is nil")
+	}
+
 	return &exchange{
 		logger: logger,
 		broker: broker,
-	}
+		store:  store,
+
+		stopSimulationChan: make(chan struct{}),
+	}, nil
 }
 
 // Main loop which handles incoming exchange bids queue messages
-func (e *exchange) recieve(ch <-chan *redis.Message) {
+func (e *exchange) recieve(ctx context.Context, ch <-chan *redis.Message) {
 	for range ch {
 		//Messages recieved from the recieve channel is from the user itself
 		fmt.Println("Message recieved from user")
@@ -71,65 +83,101 @@ func (e *exchange) recieve(ch <-chan *redis.Message) {
 	}
 }
 
-func (e *exchange) aggregate() {
+func (e *exchange) aggregate(ctx context.Context) {
+
+	timestamp := time.Now().Format(time.RFC3339)
 	//Aggregate data //TODO: I need to change the aggregate function so it only aggregates the last 5 minutes of data
-	data, err := e.store.aggregate(context.Background())
+	data, err := e.store.Aggregate(ctx, timestamp)
 	if err != nil {
 		e.logger.Error("Failed to aggregate data", zap.Error(err))
 		return
 	}
 
-	//Do some transforma
+	newPriceData := e.calculatePrice(data)
 
-	//Publish aggregated data
-	if err := e.broker.Publish(context.Background(), data); err != nil {
+	//json marshal the data
+	jsonData, err := json.Marshal(newPriceData)
+	if err != nil {
+		e.logger.Error("Failed to marshal aggregated data", zap.Error(err))
+		return
+	}
+
+	if err := e.broker.Publish(ctx, jsonData); err != nil {
 		e.logger.Error("Failed to publish aggregated data", zap.Error(err))
 		return
 	}
 
 }
 
-const (
-	intervalDuration = 1 * time.Minute
-)
-
 // Function which runs at the start of an event - the goroutine will run until the event is over
-func (e *exchange) Simulate(timestamp string) {
+func (e *exchange) Simulate(ctx context.Context, timestamp string) error {
 
 	//Here we would want to load in some configuration from the database
 	//But for now we are going to skip and use constants
+	e.conf = *newBaseConfiguration()
 
 	//Convert timestamp to time.time
 	target, err := time.Parse(time.RFC3339, timestamp)
 	if err != nil {
 		e.logger.Error("Failed to parse timestamp", zap.Error(err))
-		return
+		return err
 	}
 
 	//convert target to time.Duration
-	ctxDuration := target.Sub(time.Now())
+	ctxDuration := time.Until(target)
 
 	ctx, cancel := context.WithTimeout(context.Background(), ctxDuration)
 	defer cancel()
 
 	go func() {
+		//Subscribe to the exchange bids queuex
+		pubsub, err := e.broker.Subscribe(ctx)
+		if err != nil {
+			e.logger.Error("Failed to subscribe to exchange bids queue", zap.Error(err))
+			return
+		}
 		for {
 			select {
+
 			case <-ctx.Done():
-				fmt.Println("Context done. Stopping aggregator.")
+				e.logger.Info("Context done. Stopping recieve.")
+				e.broker.Unsubscribe(ctx, pubsub)
 				return
+
+			case <-e.stopSimulationChan:
+				e.logger.Info("Stop simulation signal recieved. Stopping recieve.")
+				e.broker.Unsubscribe(ctx, pubsub)
+				return
+
 			default:
-				e.aggregate() // Call the aggregate method on the EventAggregator instance
-				time.Sleep(time.Second)
+				//Recieve messages from the exchange bids queue
+				e.recieve(ctx, pubsub.Channel())
 			}
 		}
 	}()
 
-	//Start goroutine to aggregate data
-	go e.aggregate()
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				e.logger.Info("Context done. Stopping aggregator.")
+				return
+
+			case <-e.stopSimulationChan:
+				e.logger.Info("Stop simulation signal recieved. Stopping recieve.")
+				return
+
+			default:
+				e.aggregate(ctx)
+				time.Sleep(e.conf.intervalDuration)
+			}
+		}
+	}()
+	return nil
 }
 
 func (e *exchange) StopSimulating() {
 	//Stop goroutine to simulate price changes
 	//Stop goroutine to aggregate data
+	close(e.stopSimulationChan)
 }
