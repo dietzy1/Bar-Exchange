7fef04c8e56afb252827511a64d76112424acfee
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-09-03 09:07:31 +0200

    feat: add websocket env

--- web/src/constants/env.ts
+++ web/src/constants/env.ts
@@ -2,12 +2,14 @@
 
 interface env {
   API_URL: string;
+  WEBSOCKET_URL: string;
   MOCKED: boolean;
 }
 
 //Im lazy right now we need to read in from the .env file later
 const ENV: env = {
   API_URL: import.meta.env.VITE_API_URL as string,
+  WEBSOCKET_URL: import.meta.env.VITE_WEBSOCKET_URL as string,
   MOCKED: import.meta.env.VITE_MOCKED as boolean,
 };
 
